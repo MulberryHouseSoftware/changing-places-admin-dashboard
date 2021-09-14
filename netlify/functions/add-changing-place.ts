c0d752ace006043910ec80f3a66197d8885bb571
johnwalley <john@walley.org.uk>
2021-09-14 10:24:01 +0100

    chore: pre form hooks

---
+++ netlify/functions/add-changing-place.ts
@@ -0,0 +1,101 @@
+import { Handler } from "@netlify/functions";
+import faunadb from "faunadb";
+import { Client } from "@googlemaps/google-maps-services-js";
+
+const q = faunadb.query;
+
+const dbClient = new faunadb.Client({
+  secret: process.env.FAUNADB_ADMIN_SECRET as string,
+  domain: "db.fauna.com",
+  port: 443,
+  scheme: "https",
+});
+
+const mapsClient = new Client({});
+
+const handler: Handler = async (event, context) => {
+  if (event.body) {
+    const toilet = JSON.parse(event.body);
+    console.log(typeof toilet, toilet);
+
+    const geocode = async (toilet: any) => {
+      try {
+        let res = await mapsClient.geocode({
+          params: {
+            address: [
+              toilet.name,
+              toilet.address_1,
+              toilet.address_2,
+              toilet.city,
+              toilet.county,
+              toilet.postcode,
+              toilet.country,
+            ]
+              .filter((d) => d)
+              .join(" "),
+            key: "AIzaSyA-sTinUdfDSE7h80ld35HFS3wrRnoYgh0",
+          },
+          timeout: 1000, // milliseconds
+        });
+
+        if (res.data.results.length === 0) {
+          console.log(`No data found for ${toilet.name}`);
+        } else {
+          console.log(`Data found for ${toilet.name}`);
+        }
+
+        const result = res.data.results[0];
+
+        return {
+          place_id: result.place_id,
+          formatted_address: result.formatted_address,
+          lat: result.geometry.location.lat,
+          lng: result.geometry.location.lng,
+        };
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const result = await geocode(toilet);
+
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message:
+          "Could not geocode. Please check address information or try supplying more details",
+      }),
+    };
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ ...toilet, ...result }),
+    };
+  } else {
+    return {
+      statusCode: 500,
+    };
+  }
+
+  /* try {
+    const data = await dbClient.query<any>(
+      q.Get(q.Ref(q.Collection("changing_places"), ref)),
+      {
+        queryTimeout: 1000,
+      }
+    );
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    console.log(error);
+
+    return {
+      statusCode: 500,
+    };
+  } */
+};
+
+export { handler };
--- pages/places/add.tsx
+++ pages/places/add.tsx
@@ -1,19 +1,32 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import { FormEventHandler } from "react";
+import { FormEventHandler, useRef } from "react";
+import { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { CheckIcon, ExclamationIcon } from "@heroicons/react/outline";
+import { useRouter } from "next/router";
 
 const Add: NextPage = () => {
-  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+  const [saving, setSaving] = useState(false);
+  const [notification, setNotification] = useState<any | null>(null);
+
+  const cancelButtonRef = useRef(null);
+
+  const router = useRouter();
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
+    event.preventDefault();
+
     const target = event.target as any;
 
     const value = Object.fromEntries(
       [
         "name",
-        "address-line-1",
-        "address-line-2",
+        "address_1",
+        "address_2",
         "city",
-        "postal-code",
+        "postcode",
         "country",
         "telephone",
         "email",
@@ -23,7 +36,35 @@ const Add: NextPage = () => {
       ].map((name) => [name, target[name].value])
     );
 
-    event.preventDefault();
+    setSaving(true);
+
+    const res = await fetch("/.netlify/functions/add-changing-place/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      mode: "same-origin",
+      cache: "no-cache",
+      body: JSON.stringify(value),
+    });
+
+    const data = await res.json();
+
+    setSaving(false);
+
+    if (!res.ok) {
+      setNotification({
+        intent: "success",
+        title: `${value.name} successfully saved!`,
+        subtitle: "Anyone can now view this location in the app",
+      });
+    } else {
+      setNotification({
+        intent: "error",
+        title: "Failed to save Changing Place.",
+        subtitle: data.message,
+      });
+    }
   };
 
   return (
@@ -58,36 +99,37 @@ const Add: NextPage = () => {
                       type="text"
                       name="name"
                       id="name"
+                      required
                       className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                     />
                   </div>
 
                   <div className="col-span-6">
                     <label
-                      htmlFor="address-line-1"
+                      htmlFor="address_1"
                       className="block text-sm font-medium text-gray-700"
                     >
                       Address Line 1
                     </label>
                     <input
                       type="text"
-                      name="address-line-1"
-                      id="address-line-1"
+                      name="address_1"
+                      id="address_1"
                       className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                     />
                   </div>
 
                   <div className="col-span-6">
                     <label
-                      htmlFor="address-line-2"
+                      htmlFor="address_2"
                       className="block text-sm font-medium text-gray-700"
                     >
                       Address Line 2
                     </label>
                     <input
                       type="text"
-                      name="address-line-2"
-                      id="address-line-2"
+                      name="address_2"
+                      id="address_2"
                       className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                     />
                   </div>
@@ -109,16 +151,16 @@ const Add: NextPage = () => {
 
                   <div className="col-span-6 sm:col-span-3 lg:col-span-2">
                     <label
-                      htmlFor="postal-code"
+                      htmlFor="postcode"
                       className="block text-sm font-medium text-gray-700"
                     >
                       ZIP / Postal
                     </label>
                     <input
                       type="text"
-                      name="postal-code"
-                      id="postal-code"
-                      autoComplete="postal-code"
+                      name="postcode"
+                      id="postcode"
+                      autoComplete="postcode"
                       className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                     />
                   </div>
@@ -245,8 +287,31 @@ const Add: NextPage = () => {
                     <button
                       type="submit"
                       className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                      disabled={saving}
                     >
-                      Save
+                      {saving && (
+                        <svg
+                          className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                        >
+                          <circle
+                            className="opacity-25"
+                            cx="12"
+                            cy="12"
+                            r="10"
+                            stroke="currentColor"
+                            strokeWidth="4"
+                          ></circle>
+                          <path
+                            className="opacity-75"
+                            fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                          ></path>
+                        </svg>
+                      )}
+                      {saving ? "Saving" : "Save"}
                     </button>
                   </div>
                 </div>
@@ -255,6 +320,123 @@ const Add: NextPage = () => {
           </div>
         </div>
       </main>
+      <Transition.Root show={notification !== null} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed z-10 inset-0 overflow-y-auto"
+          initialFocus={cancelButtonRef}
+          onClose={() => {
+            setNotification(null);
+          }}
+        >
+          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className="hidden sm:inline-block sm:align-middle sm:h-screen"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+                <div>
+                  {notification?.intent === "error" ? (
+                    <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100">
+                      <ExclamationIcon
+                        className="h-6 w-6 text-red-600"
+                        aria-hidden="true"
+                      />
+                    </div>
+                  ) : (
+                    <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
+                      <CheckIcon
+                        className="h-6 w-6 text-green-600"
+                        aria-hidden="true"
+                      />
+                    </div>
+                  )}
+                  <div className="mt-3 text-center sm:mt-5">
+                    <Dialog.Title
+                      as="h3"
+                      className="text-lg leading-6 font-medium text-gray-900"
+                    >
+                      {notification?.title}
+                    </Dialog.Title>
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-500">
+                        {notification?.subtitle}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
+                  {notification?.intent === "error" ? (
+                    <>
+                      <button
+                        type="button"
+                        className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:col-start-2 sm:text-sm"
+                        onClick={() => setNotification(null)}
+                      >
+                        Update and try again
+                      </button>
+                      <Link href="/places">
+                        <a
+                          className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-1 sm:text-sm"
+                          onClick={() => setNotification(null)}
+                          ref={cancelButtonRef}
+                        >
+                          Manage Changing Places
+                        </a>
+                      </Link>
+                    </>
+                  ) : (
+                    <>
+                      <button
+                        type="button"
+                        className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:col-start-2 sm:text-sm"
+                        onClick={() => {
+                          router.reload();
+                        }}
+                      >
+                        Add another Changing Place
+                      </button>
+                      <Link href="/places">
+                        <a
+                          className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-1 sm:text-sm"
+                          onClick={() => setNotification(null)}
+                          ref={cancelButtonRef}
+                        >
+                          Manage Changing Places
+                        </a>
+                      </Link>
+                    </>
+                  )}
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition.Root>
     </div>
   );
 };

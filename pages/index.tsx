561c706cc82262345f5aea0a698bc98a16ea99fe
johnmwalley <john.walley@mulberryhousesoftware.com>
2024-05-02 09:05:30 +0100

    Fix download button styling in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,10 +46,9 @@ const Home: NextPage = () => {
                 <a
                   href="/.netlify/functions/download-changing-places"
                   download="backup.csv"
+                  className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-blue-700 bg-blue-100 hover:bg-blue-200"
                 >
-                  <a className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-blue-700 bg-blue-100 hover:bg-blue-200">
-                    Download all Changing Places
-                  </a>
+                  Download all Changing Places
                 </a>
               </div>
             </div>

e6f4732dbf88a3534e370fa80e987f06f495f3e3
johnmwalley <john.walley@mulberryhousesoftware.com>
2024-06-10 14:14:38 +0100

    chore: remove commented out code for bulk upload button in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,13 +35,13 @@ const Home: NextPage = () => {
                   </a>
                 </Link>
               </div>
-              <div className="ml-3 inline-flex">
+{/*               <div className="ml-3 inline-flex">
                 <Link legacyBehavior href="/bulk-upload">
                   <a className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-blue-700 bg-blue-100 hover:bg-blue-200">
                     Bulk upload
                   </a>
                 </Link>
-              </div>
+              </div> */}
               <div className="ml-3 inline-flex">
                 <a
                   href="/.netlify/functions/download-changing-places"
